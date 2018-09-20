055ebe364a067b304c7a031aa7b0bb94f4e6b186
Luke Harris <lukeharris01170@gmail.com>
2018-09-20 21:00:57 +0100

    Add express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log(process.env.MESSAGE);
+import express from "express";
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.json({ message: "Hello world!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
